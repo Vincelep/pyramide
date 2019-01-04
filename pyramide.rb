4c9d8a0cee500f07d193ce4e92a2944d138c07eb
vincelep <vincent.leparoux@gmail.com>
2019-01-04 23:45:22 +0100

    pyramide.rb
    git add pyramide.rb
    git commit -m pyramide.rb

---
+++ pyramide.rb
@@ -0,0 +1,13 @@
+puts "Hello, bienvenue dans ma super pyramide ! Combien d'étages tu veux ? "
+i = gets.chomp.to_i
+#j = "".to_s
+k = 0
+l = "#"
+
+puts "voici la pyramide"
+i.times do 
+#puts " #{j += "#"}"
+#end
+puts l*(k += 1)
+end
+
